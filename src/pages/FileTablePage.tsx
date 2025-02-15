1aed6052c6ae22506b3c51f4fd4336c50c511d58
ZiTe <honmonoh@gmail.com>
2025-02-15 20:25:13 +0800

    feat: select dir to read files from dialog

--- src/pages/FileTablePage.tsx
+++ src/pages/FileTablePage.tsx
@@ -1,17 +1,37 @@
+import { createSignal } from "solid-js";
+import { open } from "@tauri-apps/plugin-dialog";
+import { FileSys } from "../api/file_sys";
+import { Button } from "@suid/material";
 import FileTable from "../components/FileTable";
 
 function createData(name: string, tags: string, action: string) {
   return { name, tags, action };
 }
 
-const rows = [
-  createData("Frozen yoghurt", "T", "A"),
-  createData("Ice cream sandwich", "T", "A"),
-  createData("Eclair", "T", "A"),
-  createData("Cupcake", "T", "A"),
-  createData("Gingerbread", "T", "A"),
-];
-
 export default function FileTablePage() {
-  return <FileTable rows={rows} />;
+  const [rows, setRows] = createSignal<
+    { name: string; tags: string; action: string }[]
+  >([]);
+
+  const handleSelectDir = async () => {
+    const dir = await open({ multiple: false, directory: true });
+    if (!dir) {
+      return;
+    }
+    console.log(dir);
+
+    try {
+      const files = await FileSys.list(dir);
+      setRows(files.map((f) => createData(f, "T", "A")));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <>
+      <Button onClick={() => handleSelectDir()}>FS</Button>
+      <FileTable rows={rows()} />
+    </>
+  );
 }
