f01f05464ebb059e63acb5d87dda88bf7cec8247
ZiTe <honmonoh@gmail.com>
2025-02-15 20:10:41 +0800

    feat: install Tauri dialog plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -23,6 +23,7 @@ pub fn run() {
             Ok(())
         })
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_dialog::init())
         .invoke_handler(tauri::generate_handler![
             greet,
             exiftool::exiftool_get_version,
